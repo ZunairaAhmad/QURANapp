fd070d4586e060d243ee729e4e5b75661bd7cb1a
ZunairaAhmad <bitf19a016@pucit.edu.pk>
2023-02-16 04:37:40 -0800

    translation file added

---
+++ app/src/main/java/com/example/quranapp/Translation.java
@@ -0,0 +1,37 @@
+package com.example.quranapp;
+
+public class Translation {
+    public int number;
+    public String text;
+    public int numberInSurah;
+    public int juz;
+    public int manzil;
+    public int page;
+    public int ruku;
+    public int hizbQuarter;
+    public double sajda;
+    public int surah_number;
+    public String surah_name;
+    public String englishName;
+    public String englishNameTranslation;
+    public String revelationType;
+    public int sajda_id;
+    public int sajda_recommended;
+    public int sajda_obligatory;
+    public String UrduTranslation;
+    public String UrduTafseer;
+    public String EnglishTranslation;
+    public String Englishtafseer;
+    public String SindhiTranslation;
+    public String SindhiTafseer;
+    public String HindiTranslation;
+    public String HindiTafseer;
+    public String PushtoTransation;
+    public String PushtoTafseer;
+
+    public String getEnglishTranslation(){
+        return EnglishTranslation;
+    };
+    // Add getters and setters for all fields.
+}
+
