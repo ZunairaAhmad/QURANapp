e16c2391384b186e51db760df4bf62a5b246eaaf
ZunairaAhmad <bitf19a016@pucit.edu.pk>
2023-02-16 04:26:41 -0800

    AYA.java file added

---
+++ app/src/main/java/com/example/quranapp/Aya.java
@@ -0,0 +1,66 @@
+package com.example.quranapp;
+
+public class Aya {
+    public int number;
+    public String text;
+    public int numberInSurah;
+    public int juz;
+    public int manzil;
+    public int page;
+    public int ruku;
+    public int hizbQuarter;
+    public double sajda;
+    public int surahNumber;
+    public String surahName;
+    public String englishName;
+    public String englishNameTranslation;
+    public String revelationType;
+    public String sajdaId;
+    public String sajdaRecommended;
+    public String sajdaObligatory;
+    public String urduTranslation;
+    public String urduTafseer;
+    public String englishTranslation;
+    public String englishTafseer;
+    public String sindhiTranslation;
+    public String sindhiTafseer;
+    public String hindiTranslation;
+    public String hindiTafseer;
+    public String pushtoTranslation;
+    public String pushtoTafseer;
+
+    public Aya(int number, String text, int numberInSurah, int juz, int manzil, int page, int ruku, int hizbQuarter, double sajda, int surahNumber, String surahName, String englishName, String englishNameTranslation, String revelationType, String sajdaId, String sajdaRecommended, String sajdaObligatory, String urduTranslation, String urduTafseer, String englishTranslation, String englishTafseer, String sindhiTranslation, String sindhiTafseer, String hindiTranslation, String hindiTafseer, String pushtoTranslation, String pushtoTafseer) {
+        this.number = number;
+        this.text = text;
+        this.numberInSurah = numberInSurah;
+        this.juz = juz;
+        this.manzil = manzil;
+        this.page = page;
+        this.ruku = ruku;
+        this.hizbQuarter = hizbQuarter;
+        this.sajda = sajda;
+        this.surahNumber = surahNumber;
+        this.surahName = surahName;
+        this.englishName = englishName;
+        this.englishNameTranslation = englishNameTranslation;
+        this.revelationType = revelationType;
+        this.sajdaId = sajdaId;
+        this.sajdaRecommended = sajdaRecommended;
+        this.sajdaObligatory = sajdaObligatory;
+        this.urduTranslation = urduTranslation;
+        this.urduTafseer = urduTafseer;
+        this.englishTranslation = englishTranslation;
+        this.englishTafseer = englishTafseer;
+        this.sindhiTranslation = sindhiTranslation;
+        this.sindhiTafseer = sindhiTafseer;
+        this.hindiTranslation = hindiTranslation;
+        this.hindiTafseer = hindiTafseer;
+        this.pushtoTranslation = pushtoTranslation;
+        this.pushtoTafseer = pushtoTafseer;
+    }
+
+    public Aya(){
+
+    }
+}
+
